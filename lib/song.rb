5ff93cd47b571a9a965fb28c23d76d0ced6f24e7
Nina Kazzim <kazzim@icloud.com>
2019-07-28 20:46:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
 
   def self.all
     @@all
@@ -9,5 +10,47 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(string_name)
+    song = self.new 
+    song.name = string_name
+    song
+  end
+  
+  def self.create_by_name(create_name)
+    song = self.new 
+    song.name = create_name
+    song.save 
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.each do |song|
+      if song.name == name
+        return song
+      end 
+    end
+  end 
+  
+  # def self.find_or_create_by_name(new_string_name)
+  #   self.find_by_name(new_string_name) || self.create_by_name(new_string_name)
+  # end
+  def self.find_or_create_by_name(name)
+     find_it = self.all.detect {|x| x.name == song}
+    if find_it == nil
+      s = self.new 
+      s.name = song
+      s.save 
+      s
+    else
+      find_it
+    end
+  end
+  
 end
